afb2523cafba16c34892ba83becb6ef153ef122d
Lupilgaming <65975615+Lupilgaming@users.noreply.github.com>
2020-10-26 15:34:20 +0530

    haz solving file

---
+++ haz.js
@@ -0,0 +1,100 @@
+class term{
+
+  constructor(str){
+		this.n = str.length;
+		this.content = str.toString().valueOf();
+
+	}
+}
+
+class Kmap{
+
+	constructor(l1){
+		this.terms = [];
+		for(let i in l1){
+			this.terms[i] = l1[i];
+		}
+		this.t_n = this.terms.length;
+		this.hz_solution = [];
+	}
+
+	hzsolve(){
+		for(let i = 0; i < this.t_n; i++){
+			var term1 = this.terms[i];
+			for(let j = i+1; j < this.t_n; j++){
+				var term2 = this.terms[j];
+
+				this.generate(term1, term2);
+				
+			}
+		}
+	}
+
+	is_adjacent(t1, t2){
+		var n = t1.n;
+		var sum = 0;
+		for(let i = 0; i < n; i++){
+			// check each character of both term contents
+			let c1 = t1.content[i];
+			let c2 = t2.content[i];
+
+			if(c1 == '_' || c2 == '_')
+				sum = sum + 0;
+			else if(c1 != c2){
+				sum = sum + 1;
+			}
+			else ;
+		}
+		var val = "";
+		if(sum == 1){
+
+			for(let i = 0; i < n; i++){
+			// check each character of both term contents
+			let c1 = t1.content[i];
+			let c2 = t2.content[i];
+
+			if(c1 == '_' || c2 == '_'){
+				if(c1 == c2)
+					val = val + '_';
+				else {
+					if(c1 == '_')
+						val =val + c2;
+					else 
+						val = val + c1;
+				}
+			}
+			else{
+				if(c1 == c2)
+					val = val + c1;
+				else val = val + '_';
+			}
+			}
+
+			return([1, val]);
+		}
+		else {
+			return([0, 0]);
+		}
+	}
+
+	generate(t1, t2){
+		let ret, value;
+		[ret, value] = this.is_adjacent(t1, t2);
+		if(ret == 1){
+			this.hz_solution.push(value);
+			console.log(value);
+		}
+		else {
+			console.log(0);
+		}
+		
+	}
+}
+/*
+
+running
+ter1 = new term("11_");
+ter2 = new term("0_1");
+k = new Kmap([ter1, ter2]);
+k.hzsolve()
+*/
